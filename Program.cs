19b9f0fcfe3af0e86ac942377ea5813076515b5e
anatolii <anatoliise@mail.ru>
2022-02-27 21:47:13 +0300

    задача1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("введите первое число ");
+string strNoumberA = Console.ReadLine();
+int NoumberA = int.Parse(strNoumberA);
+
+Console.WriteLine("введите второе число ");
+string strNoumberB = Console.ReadLine();
+int NoumberB = int.Parse(strNoumberB);
+
+if (NoumberA > NoumberB)
+{
+    Console.WriteLine(NoumberA);
+}
+if (NoumberA < NoumberB)
+{
+    Console.WriteLine(NoumberB);
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
